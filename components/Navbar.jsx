6b3f35ed046c73747f9f6178c7819ff7a3ad7743
Consoling <shahidarizfannymagnet@gmail.com>
2023-03-10 20:07:35 +0530

    modified:   components/Cart.jsx 	modified:   components/Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,18 +7,19 @@ import {useStateContext} from '../context/StateContext';
 
 
 const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities} = useStateContext();
   return (
     <div className="navbar-container">
       <p className='logo'>
       <Link href="/">GOAT Store</Link>
       </p>
       <button type="button"
-      className='cart-icon' onClick="">
+      className='cart-icon' onClick={() => setShowCart(true)}>
       <AiOutlineShopping />
-      <span className="cart-item-qty">1</span>
+      <span className="cart-item-qty">{totalQuantities}</span>
       </button>
 
-      <Cart />
+      {showCart && <Cart />}
     </div>
   )
 }
