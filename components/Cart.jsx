afa1fec742aaf5e706b2bb7f55b2fb04c2dd44d4
Consoling <shahidarizfannymagnet@gmail.com>
2023-03-11 19:21:43 +0530

    modified:   components/Cart.jsx 	new file:   lib/getStripe.js 	modified:   package-lock.json 	modified:   package.json 	new file:   pages/api/stripe.js

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -6,14 +6,33 @@ import toast from 'react-hot-toast';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
-// import getStripe from '../lib/getStripe';
-
-
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
 
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+    
+    const data = await response.json();
+
+    console.log(data);
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -59,9 +78,7 @@ const Cart = () => {
                     <AiOutlineMinus />
                     </span>
                     <span className="num" onClick="">{item.quantity}</span>
-                    <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }>
-                    <AiOutlinePlus />
-                    </span>
+                    <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc') }><AiOutlinePlus /></span>
                   </p>
                   </div>
                   <button
@@ -75,20 +92,20 @@ const Cart = () => {
               </div>
             </div>
           ))}
-      </div>
-      {cartItems.length >= 1 && (
-        <div className='cart-bottom'> 
-        <div className="total">
-        <h3>Subtotal: </h3>
-        <h3>${totalPrice}</h3>
         </div>
-        <div className='btn-container'>
-        <button type="button" className='btn' onClick="">
-        Pay with Stripe
-        </button>
-        </div>
-        </div>
-      )}
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className="btn" onClick={handleCheckout}>
+                Pay with Stripe
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
